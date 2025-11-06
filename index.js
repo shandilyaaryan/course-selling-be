49af6b6cca8d9abd93defbca5e22d18d24a4c92c
Aryan <aryanshandilya10@gmail.com>
2025-11-06 11:40:20 +0530

    Added admin routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import courseRouter from "./routes/course.js";
+import adminRouter from "./routes/admin.js";
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
 
 app.listen(3000, console.log("Server is running"));
---
+++ routes/admin.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", (req, res) => {
+  res.send("Admin signup");
+});
+
+adminRouter.post("/signin", (req, res) => {
+  res.send("Admin signin");
+});
+
+adminRouter.post("/delete", (req, res) => {
+  res.send("Admin delete course");
+});
+
+adminRouter.post("/course", (req, res) => {
+  res.send("Admin create course");
+});
+
+adminRouter.put("/course", (req, res) => {
+  res.send("Admin add");
+});
+
+adminRouter.post("/course/bulk", (req, res) => {
+  res.send("These are the courses");
+});
+
+export default adminRouter;
