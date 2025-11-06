be2cd41f8412b374e8790da1e7b158676ea2ed53
Aryan <aryanshandilya10@gmail.com>
2025-11-06 10:32:51 +0530

    Added node modules to gitignore

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(json());
 
 app.get("/login", (req, res) => {});
-app.get("/signup", (req, res) => {});
-app.get("/purchase", (req, res) => {});
-app.get("/get", (req, res) => {});
-
+app.post("/signup", (req, res) => {});
+app.get("/courses", (req, res) => {});
+app.put("/course/purchase", (req, res) => {});
+app.get("/user/courses", (req, res) => {});
 app.listen(3000);
